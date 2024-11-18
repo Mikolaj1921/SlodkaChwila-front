a339894a42ba5e0be12e222da3e9b795cbc3dd1b
Mikolaj231 <mikolajmelnyk16@gmail.com>
2024-11-19 00:41:47 +0100

    Aktualizacja 1.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';  // Importujemy Link z react-router-dom
 import './Header.css';
 
 const Header = () => {
+  // Stan kontrolujący widoczność menu na urządzeniach mobilnych
+  const [menuActive, setMenuActive] = useState(false);
+
+  // Funkcja do przełączania widoczności menu
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header>
       <nav>
-        {/* Dodajemy linki do różnych stron */}
-        <ul className="nav-links">
+        {/* Ikona menu, widoczna na urządzeniach mobilnych */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776; {/* Symbol hamburgera */}
+        </div>
+        
+        {/* Menu nawigacyjne */}
+        <ul className={`nav-links ${menuActive ? 'active' : ''}`}>
           <li><Link to="/">Strona domowa</Link></li>  {/* Link do strony Home */}
           <li><Link to="/contact">Kontakt</Link></li>  {/* Link do strony Contact */}
           <li><Link to="/menu">Menu</Link></li>  {/* Link do strony Menu */}
