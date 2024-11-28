6c737a663022bbe0df68b16725d018df92e9be95
Mikolaj231 <mikolajmelnyk16@gmail.com>
2024-11-28 14:03:11 +0100

    Aktualizacja 2.0 - animacje menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import './Home.css'; // Importujemy zaktualizowane style
-import backgroundImage from '../components/images/dohome.jpg';
+//import backgroundImage from '../components/images/dohome.jpg';
+
+//Górne tło pod nagłówkiem(przewijanie się zależne od scroolingu)
+window.addEventListener('scroll', function() {
+  const scrollPosition = window.scrollY; // Pobierz pozycję scrolla
+  const menuCont = document.querySelector('.menu-cont');
+  
+  // Zmieniamy pozycję tła w zależności od scrolla
+  menuCont.style.backgroundPosition = `center ${50 - scrollPosition * 0.2}%`;
+});
+
 
 const Home = () => {
   return (
     <div className="home-page">
-      <div className="menu-cont" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="menu-cont">
+        <div className="title-names">
+          <h1 className="hero-title">Witaj w Słodkiej Chwili!</h1>
+          <p className="hero-subtitle">Ciepłe, domowe wypieki na każdą okazję</p>
+        </div>
       </div>
       
-      <div className="title-names">
-        <h1 className="hero-title">Witaj w Słodkiej Chwili!</h1>
-        <p className="hero-subtitle">Ciepłe, domowe wypieki na każdą okazję</p>
-      </div>
+      
 
       <section className="about-section">
         <h2 className="section-title">Nasze wypieki:</h2>
+        <div className="containerWypieki">
+          <div className="card1">
+            <button>Ciasta na każdą okazję</button>
+          </div>
+          <div className="card2">
+            <button>Świeże ciasteczka i ciasteczka</button>
+          </div>
+          <div className="card3">
+            <button>Desery bezglutenowe</button>
+          </div>
+
+        </div>
+
+        {/*
         <ul className="service-list">
           <li>Ciasta na każdą okazję</li>
           <li>Świeże ciasteczka i ciasteczka</li>
@@ -22,6 +47,8 @@ const Home = () => {
           <li>Cukierki i praliny</li>
           <li>Wypieki sezonowe</li>
         </ul>
+        */}
+
       </section>
 
       <section className="why-us-section">
